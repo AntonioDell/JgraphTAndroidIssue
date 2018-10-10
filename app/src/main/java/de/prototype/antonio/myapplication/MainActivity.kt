0c808e68bb0d486ad1a2da17aa0431d9088b44cc
AntonioDell <Antonio.Dell@gmx.de>
2018-10-10 17:52:07 +0200

    Remove JGraphT dependency -> works now
    The problem obviously was only introduced by the library, since the project
    works without it.
    
    This branch should build and execute normally with Android Studio 3.2 on
    Android API 26

--- app/src/main/java/de/prototype/antonio/myapplication/MainActivity.kt
+++ app/src/main/java/de/prototype/antonio/myapplication/MainActivity.kt
@@ -2,11 +2,8 @@ package de.prototype.antonio.myapplication
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import org.jgrapht.Graph
 
 class MainActivity : AppCompatActivity() {
-
-    var graph: Graph<Int, Int>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
